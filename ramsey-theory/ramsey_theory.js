af1324795f823c24d84099afa3ffae53d6f90a7e
Hubert Puszklewicz <hubert.puszklewicz@stonybrook.edu>
2018-07-15 05:17:07 -0400

    finding all triangles, detecting unicolored triangles

--- ramsey-theory/ramsey_theory.js
+++ ramsey-theory/ramsey_theory.js
@@ -40,6 +40,9 @@ pb.draw = function (floor, p) {
 	}
 	console.log(listOfPairs.length);
     }
+
+    var tris = getAllTriangles();
+    console.log("Num of all tris: " tris.length);
     
     // for(var i = 0; i < listOfPairs.length; i++){
     //   var obj = listOfPairs[i];
@@ -48,6 +51,13 @@ pb.draw = function (floor, p) {
     //   console.log(obj["color"]);
     // }
 
+    var someUnicolorTri = undefined;
+    for(var i = 0; i <= tris.length; i++){
+	if(triangleUnicolor(tris[i])){
+	    console.log("UNICOLOR TRIANGLE");
+	    someUnicolorTri = tris[i];
+	}
+    }
     
     for(var i = 0; i < listOfPairs.length; i++){
 	var pair = listOfPairs[i];
@@ -56,17 +66,52 @@ pb.draw = function (floor, p) {
 	var color = pair["color"];
 	this.stroke(color);
 	this.line(u1.x, u1.y, u2.x, u2.y);
-	colorTriangle();
 	pb.drawUser(u1);
     }
     
     frameNum+=1;
 };
 
-function colorTriangle(){
-    
+//gT is short for getTriangle
+function gT(a, b, c){
+    return [listOfPairs[a], listOfPairs[b], listOfPairs[c]];
+}
 
-};
+function getAllTriangles(){
+    //tL is short for triangleList
+    var tL = [];
+    tL.push(gT(1,2,8));
+    tL.push(gT(2,3,15));
+    tL.push(gT(3,4,22));
+    tL.push(gT(4,5,29));
+    tL.push(gT(8,9,15));
+    tL.push(gT(9,10,22));
+    tL.push(gT(10,11,29));
+    tL.push(gT(15,16,22));
+    tL.push(gT(16,17,29));
+    tL.push(gT(22,23,29));
+    tL.push(gT(5,2,17));
+    tL.push(gT(1,4,10));
+    tL.push(gT(2,4,16));
+    tL.push(gT(3,5,23));
+    tL.push(gT(1,3,9));
+    tL.push(gT(4,1,10));
+    tL.push(gT(5,1,11));
+    tL.push(gT(1,3,9));
+    tL.push(gT(2,4,16));
+    tL.push(gT(9,11,23));
+    return tL;
+}
+
+function triangleUnicolor(tri){
+    try{
+	var bool = ((tri[0]["color"] == tri[1]["color"]) && (tri[1]["color"] == tri[2]["color"]));
+    }
+    catch(err){
+	return false;
+    }
+    return bool;
+}
 
 
 export const behavior = {
